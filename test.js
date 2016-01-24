01474ed698adb179c59f8504c576ec56529eff01
Lucas <AgtLucas@users.noreply.github.com>
2016-01-24 08:34:17 -0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-Rx.Observable.timer(0, 1000)
-  .map(i => `Seconds elapsed ${i}`)
-  .subscribe(text => {
+// // Logical (functional)
+// Rx.Observable.timer(0, 1000)
+//   .map(i => `Seconds elapsed ${i}`)
+// // Effects (imperative)
+//   .subscribe(text => {
+//     const container = document.querySelector('#app')
+//     container.textContent = text
+//   })
+
+function main () {
+  return Rx.Observable.timer(0, 1000)
+    .map(i => `Seconds elapsed ${i}`)
+}
+
+function DOMEffect(text$) {
+  text$.subscribe(text => {
     const container = document.querySelector('#app')
     container.textContent = text
   })
+}
